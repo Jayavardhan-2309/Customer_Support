3714dd05c53af5f1282bd1a445e6a1133e560166
Jayavardhan-2309 <n.jayavardhan2309@gmail.com>
2026-03-30 13:02:21 +0530

    removed mail from celery v2

--- backend/custSupApp/tasks.py
+++ backend/custSupApp/tasks.py
@@ -10,7 +10,7 @@ logger = get_task_logger(__name__)
 
 
 #@shared_task(bind=True, max_retries=3, default_retry_delay=10)
-def send_ticket_email(self, ticket_id, staff_email, conversation_text, query):
+def send_ticket_email(ticket_id, staff_email, conversation_text, query):
     try:
         ticket = SupportTicket.objects.get(id=ticket_id)
 
