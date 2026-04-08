5ab7537824b5bd9cc2ad0468efd0503f4077b33f
Jayavardhan-2309 <n.jayavardhan2309@gmail.com>
2026-04-08 10:56:20 +0530

    logger env added

--- frontend/logger.ts
+++ frontend/logger.ts
@@ -1,5 +1,5 @@
 // use this file for production logging
-const isProd= true
+const isProd= process.env.NODE_ENV === "production"
 export const logger={
     info: (msg: string, ...args: any[]) => !isProd && console.log(`[INFO] ${msg}`, ...args),
     warn: (msg: string, ...args: any[]) => console.warn(`[WARN] ${msg}`, ...args),
