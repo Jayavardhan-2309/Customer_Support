d274a7277b6d6dfcc537d58bba976e6f082db216
Jayavardhan-2309 <n.jayavardhan2309@gmail.com>
2026-04-20 17:01:46 +0530

    allowing safe http methods

--- backend/custSupport/views.py
+++ backend/custSupport/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse, require_GET
-from django.views import api_view
+from rest_framework.decorators import api_view
 
 @require_GET
 @api_view(['GET'])
-def backend_res(_request): # using _ before request or any other parameter makes the warning of unused parameter go away, so _request is good but just request raises warning by ide
+def backend_res(_request): # using _ before request or any other parameter makes the warning of unused parameter go away, so _request is good but just that request raises warning by ide without _
     return HttpResponse('<h1>backend working</h1>')
 
