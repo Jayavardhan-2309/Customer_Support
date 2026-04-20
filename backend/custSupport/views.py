08a49019f4413d7116370d78e403170455e3de20
Jayavardhan-2309 <n.jayavardhan2309@gmail.com>
2026-04-20 16:45:10 +0530

    used require_GET for security hotspot

--- backend/custSupport/views.py
+++ backend/custSupport/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, require_GET
 
+@require_GET
 def backend_res(_request): # using _ before request or any other parameter makes the warning of unused parameter go away, so _request is good but just request raises warning by ide
     return HttpResponse('<h1>backend working</h1>')
 
