7240d781beaad71b2349a8cec03af034248d96ca
Jayavardhan-2309 <n.jayavardhan2309@gmail.com>
2026-04-21 15:17:19 +0530

    backend page permission

--- backend/custSupport/views.py
+++ backend/custSupport/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.views.decorators.http import require_GET
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 
 @require_GET
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def backend_res(_request): # using _ before request or any other parameter makes the warning of unused parameter go away, so _request is good but just that request raises warning by ide without _
     return HttpResponse('<h1>backend working</h1>')
 
