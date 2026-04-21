f1e97029127ed52c71c745812c212919eb500c54
Jayavardhan-2309 <n.jayavardhan2309@gmail.com>
2026-04-21 14:16:44 +0530

    coverage v2.2

--- backend/custSupport/test_settings.py
+++ backend/custSupport/test_settings.py
@@ -1,11 +1,13 @@
-import os
+from . import settings as base_settings
 
-from .settings import *  # noqa: F403,F401
+for setting_name in dir(base_settings):
+    if setting_name.isupper():
+        globals()[setting_name] = getattr(base_settings, setting_name)
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "test_db.sqlite3",  # noqa: F405
+        "NAME": BASE_DIR / "test_db.sqlite3",
     }
 }
 
