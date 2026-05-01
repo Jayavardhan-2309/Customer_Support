1f5aea89e8b47d0dec94aa01c134f19973378f49
Jayavardhan-2309 <n.jayavardhan2309@gmail.com>
2026-05-01 10:41:15 +0530

    deployment changes for new code

--- backend/manage.py
+++ backend/manage.py
@@ -2,10 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
 
 
 def main():
     """Run administrative tasks."""
+    BASE_DIR = Path(__file__).resolve().parent
+    sys.path.insert(0, str(BASE_DIR))
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'custSupport.settings')
     try:
         from django.core.management import execute_from_command_line
